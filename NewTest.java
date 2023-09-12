4c5ae11908b5eb045614c74e712c3f79ffe90dc8
aryanstm <98084541+aryanstm@users.noreply.github.com>
2023-09-12 12:39:25 +0530

    Add files via upload

---
+++ NewTest.java
@@ -0,0 +1,121 @@
+package com.amdocs.selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+	WebDriver driver ;
+	@Test
+  public void f() {
+		System.out.println("Selecting the gender");
+		driver.findElement(By.id("male")).click();
+
+		System.out.println("Choosing the food preference");
+		driver.findElement(By.id("veg")).click();
+
+		WebElement countryDropdown = driver.findElement(By.id("countries"));
+		Select dropdown = new Select(countryDropdown);
+		System.out.println("Choosing country from dropdown menu");
+		dropdown.selectByValue("india");
+
+ 
+ 
+        WebElement login = driver.findElement(By.id("signin-button"));
+        System.out.println("Clicking on the login element in the main page");
+        login.click();
+ 
+        driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
+ 
+        WebElement email = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("signin-button"));
+        Actions builder = new Actions(driver);
+        builder.moveToElement(email).perform();
+ 
+        email.clear();
+        System.out.println("Entering the email");
+        email.sendKeys("himanshu@email.com");
+ 
+        password.clear();
+        System.out.println("entering the password");
+        password.sendKeys("password@123");
+        WebElement gender = driver.findElement(By.id("male"));
+		boolean selectState = gender.isSelected();
+		if (selectState == false) {
+			gender.click();
+		}
+		WebElement food = driver.findElement(By.id("veg"));
+		boolean isSelected = food.isSelected();
+		if (isSelected == false) {
+			food.click();
+		}
+
+		WebElement country = dropdown.getFirstSelectedOption();
+		String selectedoption = country.getText();
+		System.out.println(selectedoption);
+
+		if (selectedoption == "India") {
+			System.out.println("Correct option");
+		}
+ 
+        System.out.println("Clicking login button");
+        loginButton.click();
+ 
+        String title = "Welcome - LambdaTest";
+ 
+        String actualTitle = driver.getTitle();
+ 
+        System.out.println("Verifying the page title has started");
+        Assert.assertEquals(actualTitle,title);
+ 
+        System.out.println("The page title has been successfully verified");
+ 
+        System.out.println("User logged in successfully");
+ 
+
+	
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+		// Path of chrome driver
+		// that will be local directory path passed
+		System.setProperty("webdriver.chrome.driver",
+				"C://Users//aryan/Downloads//chromedriver-win64//chromedriver-win64//chromedriver.exe");
+		// URL of the login website that is tested
+		driver = new ChromeDriver();
+      String url = "file:///C:/Users/aryan/Downloads/Archive/login.html";
+      driver.get(url);
+
+     
+      driver.manage().window().maximize();
+      driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+      driver.quit();
+
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
